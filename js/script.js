0e7b0e43cf796377dcdf36dddb607e0c14e8d1ce
Maksemys <makcemyc@gmail.com>
2024-02-08 20:29:26 +0200

    some example

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
-'use strict';
+// Пример 1
+// const hamburger = true;
+// const fries = true;
 
-if (4 == 9) {
-	console.log('Ok!');
-} else {
-	console.log('Error');
-}
-
-// const num = 50;
-
-// if (num < 49) {
-// 	console.log('Error');
-// } else if (num > 100) {
-// 	console.log('Много');
-// } else {
-// 	console.log('Ok');
+// if (hamburger && fries) {
+// 	console.log('Я сыт');
 // }
 
-// (num === 50) ? console.log('Ok') : console.log('Error');
+// console.log((hamburger && fries)); 
 
-const num = 50;
 
-switch (num) {
-	case 49:
-		console.log('Неверно');
-		break;
-	case 100:
-		console.log('Неверно');
-		break;
-	case 50:
-		console.log('В точку!');
-		break;
-	default: 
-	console.log('Не в этот раз');
-	break;
+// Пример 2
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// if (hamburger === 3 && cola === 1 && fries){
+// 	console.log('Все сыты')
+// } else {
+// 	console.log('Мы уходим')
+// }
+
+// Пример 3
+const hamburger = 0;
+const fries = null;
+const cola = 0;
+
+if (hamburger || cola || fries) {
+	console.log('Все довольні')
+} else {
+	console.log('Мы уходим')
 }
+
+console.log(hamburger || cola || fries)
